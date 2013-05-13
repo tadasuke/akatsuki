8648beccd3e05a2fd455b1f619b70d977e827625
tadasuke <tadasuketadasuke@gmail.com>
2013-05-13 19:36:24 +0900

    設定ファイルクラス追加

---
+++ AK_Ini.php
@@ -0,0 +1,22 @@
+<?php
+
+class AK_Ini {
+	
+	/**
+	 * 設定をマージする
+	 * @param array $fromArray
+	 * @param array $toArray
+	 */
+	protected function mergeConfig( array &$fromArray, array &$toArray ) {
+	
+		foreach ( $fromArray as $key => $value ) {
+			if ( is_array( $fromArray[$key] ) === TRUE ) {
+				$this -> mergeConfig( $fromArray[$key], $toArray[$key] );
+			} else {
+				$toArray[$key] = $fromArray[$key];
+			}
+		}
+	
+	}
+	
+}
