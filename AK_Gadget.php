56123c7d533a35e0ab949cf0fbeabef871198c12
tadasuke <tadasuketadasuke@gmail.com>
2013-05-14 15:52:45 +0900

    ライブラリ追加

--- AK_Gadget.php
+++ AK_Gadget.php
@@ -13,5 +13,33 @@ class AK_Gadget {
 		
 	}
 	
+	/**
+	 * 残り時間取得処理
+	 * YmdHis形式の時刻の差を秒数で求める
+	 * @param int $nowDate
+	 * @param int $oldDate
+	 * @return int $seconds;
+	 */
+	public static function getLastSeconds( $newDate, $oldDate = NULL ) {
+		$newTime = strtotime( $newDate );
+		$oldTime = (is_null( $oldDate )) ? time() : strtotime( $oldDate );
+		return $newTime - $oldTime;
+	}
+	
+	/**
+	 * 連想配列の特定のキーを元にソートする
+	 * @param array $array
+	 * @param string $sortKey
+	 * @param int $sortType
+	 */
+	public static function sortArrayByKey( &$array, $sortKey, $sortType = SORT_ASC ) {
+	
+		$tmpArray = array();
+		foreach ( $array as $key => $row ) {
+			$tmpArray[$key] = $row[$sortKey];
+		}
+		array_multisort( $tmpArray, $sortType, $array );
+		unset( $tmpArray );
+	}
 	
 }
