1bd16746b93bc36ff02790aa30064c19ef9b05cb
fmcalsys <game-tech.server@bitst.jp>
2015-06-11 17:56:12 +0900

    AK_DateTimeに差分を秒数で返すメソッド追加

--- AK_DateTime.php
+++ AK_DateTime.php
@@ -50,4 +50,15 @@ class AK_DateTime extends DateTime {
 		
 	}
 	
+	
+	/**
+	 * 差分を秒数で返す
+	 * @param AK_DateTime $dateTime
+	 */
+	public function getDiffSeconds( AK_DateTime $dateTime ) {
+		
+		return $dateTime -> format( 'U' ) - $this -> format( 'U' ); 
+		
+	}
+	
 }
